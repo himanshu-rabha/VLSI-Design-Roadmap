19a9f5eeee8d9755446c058c61483913b072185b
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 15:31:17 +0530

    Update and_gate.v

--- Day_1_Basics_gates/RTL/and_gate.v
+++ Day_1_Basics_gates/RTL/and_gate.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 13:56:14
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input a,input b,output c
+    );
+    
+    assign
+     c = a & b;
+endmodule
 
