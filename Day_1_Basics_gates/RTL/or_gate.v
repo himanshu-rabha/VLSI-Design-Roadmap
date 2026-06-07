4c92edfbc64446217dbc56fd813c7b1f597b5a27
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 15:33:05 +0530

    Update or_gate.v

--- Day_1_Basics_gates/RTL/or_gate.v
+++ Day_1_Basics_gates/RTL/or_gate.v
@@ -1,1 +1,31 @@
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 14:13:19
+// Design Name: 
+// Module Name: or_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or_gate(
+    input a,
+    input b,
+    output c
+    );
+    
+    assign c = a | b;
+endmodule
