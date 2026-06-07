27443389dbf4a33ce05a8852d2f179dc6e2b4d23
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 15:40:30 +0530

    Create tb_or_gate.v

---
+++ Day_1_Basics_gates/Testbench/tb_or_gate.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 14:14:41
+// Design Name: 
+// Module Name: tb_or_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_or_gate;
+   
+    reg a;
+    reg b;
+    
+    wire c;
+    
+    or_gate uut(
+    .a(a),
+    .b(b),
+    .c(c)
+    );
+    
+    initial begin
+    
+    a=0; b=0;
+    #10;
+    
+    a=0; b=1;
+    #10;
+    
+    a=1; b=0;
+    #10;
+    
+    a=1; b=1;
+    #10;
+    
+    $finish;
+    
+    end
+endmodule
