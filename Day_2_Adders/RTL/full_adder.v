6757f2c2e7e31b6d7d6df644069a62b3db7c2da7
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 18:29:23 +0530

    Create full_adder.v

---
+++ Day_2_Adders/RTL/full_adder.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 17:40:26
+// Design Name: 
+// Module Name: full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+    );
+    
+    assign sum = a^b^cin;
+    assign cout= (a & b) | (cin & (a ^ b));
+endmodule
