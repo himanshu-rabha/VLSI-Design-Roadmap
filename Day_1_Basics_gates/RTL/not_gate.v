57ed58af041f4f299813745bc3ccf60065c558c8
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 15:34:04 +0530

    Update not_gate.v

--- Day_1_Basics_gates/RTL/not_gate.v
+++ Day_1_Basics_gates/RTL/not_gate.v
@@ -1,1 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 14:27:50
+// Design Name: 
+// Module Name: not_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module not_gate(
+    input a,
+    output b
+    );
+    
+    assign b = ~a;
+endmodule
