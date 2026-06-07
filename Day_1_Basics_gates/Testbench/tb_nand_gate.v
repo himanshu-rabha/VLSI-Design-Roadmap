2aa211ac085cc7e413b542a8e67c4a1162e05a29
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-07 15:42:14 +0530

    Create tb_nand_gate.v

---
+++ Day_1_Basics_gates/Testbench/tb_nand_gate.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 14:57:53
+// Design Name: 
+// Module Name: tb_nand_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_nand_gate;
+    reg a;
+    reg b;
+    wire c;
+    
+    nand_gate uut(
+        .a(a),
+        .b(b),
+        .c(c)
+    
+    );
+    
+    initial begin
+    
+    a=0; b=0;
+    #10;
+    
+    a=0; b=1;
+    #10;
+    
+    a=1; b=0;
+    #10;
+    
+    a=1; b=1;
+    #10;
+    
+    $finish;
+    end
+    
+endmodule
