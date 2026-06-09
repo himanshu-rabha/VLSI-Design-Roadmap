2ea961f8d52b43cc52e7fae129b3a0d946e00928
Himanshu Rabha <himanshurabhadesign@gmail.com>
2026-06-10 01:43:34 +0530

    Create tb_decoder_3to8.v

---
+++ Day_4_Decoder_Encoder/Testbench/tb_decoder_3to8.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+module tb_decoder_3to8;
+
+    reg a;
+    reg b;
+    reg c;
+    
+    wire y0;
+    wire y1;
+    wire y2;
+    wire y3;
+    wire y4;
+    wire y5;
+    wire y6;
+    wire y7;
+    
+    decoder_3to8 utt(
+        
+        .a(a),
+        .b(b),
+        .c(c),
+        .y0(y0),
+        .y1(y1),
+        .y2(y2),
+        .y3(y3),
+        .y4(y4),
+        .y5(y5),
+        .y6(y6),
+        .y7(y7)
+    );
+    
+    initial begin
+    
+    a=0; b=0; c=0; #10;
+    a=0; b=0; c=1; #10;
+    a=0; b=1; c=0; #10;
+    a=0; b=1; c=1; #10;
+    a=1; b=0; c=0; #10;
+    a=1; b=0; c=1; #10;
+    a=1; b=1; c=0; #10;
+    a=1; b=1; c=1; #10;
+    
+    $finish;
+    end
+endmodule
